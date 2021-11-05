9acb4ba0d1c88be27848b991c10228ec46aa26f5
midosdm <mohamedsaidem01@gmail.com>
2021-11-05 02:24:31 +0100

    cleaning up the code

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import gamesReducer from '../features/games/gamesSlice';
-
-export const store = configureStore({
-    reducer:{
-        games: gamesReducer
-    }
-})
--- src/components/games/Games.jsx
+++ src/components/games/Games.jsx
@@ -1,20 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import './games.css';
-// import {useSelector, useDispatch} from 'react-redux';
-// import {
-//     gamesAsync,
-//     gamesSelector,
-//     selectGame
-// } from '../../features/games/gamesSlice';
 import useFetch from '../../custom/useFetch';
 
 
 const Games = ({handleChange}) => {
-    // const {selectedGame, games} = useSelector(gamesSelector);
-    // const dispatch = useDispatch();
-    // useEffect(() =>{
-    //     dispatch(gamesAsync());
-    // }, [dispatch])
     const {data:games} = useFetch(process.env.REACT_APP_VIDEOGAMES_API_URL);
 
     return (
@@ -41,11 +30,6 @@ const Games = ({handleChange}) => {
             </>
             }
             </select>
-            {/* <ContextSelectedGame.Provider value={{selectedGame, setSelectedGame}}>
-                <div>
-                    <LeagueList />
-                </div>
-            </ContextSelectedGame.Provider> */}
         </>
     )
 }
